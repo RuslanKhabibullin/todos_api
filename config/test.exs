aabc38169fc82c2dcf9f495a9b733272e5aa1008
RuslanKhabibullin <khabibullin.ruslan.95@gmail.com>
2019-10-01 23:53:55 +0500

    fix test config for CI database connection

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,11 @@ config :todos_api, TodosApi.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+if postgres_user = System.get_env("POSTGRES_USER") do
+  config :todos_api, TodosApi.Repo, username: postgres_user
+  config :todos_api, TodosApi.Repo, password: System.get_env("POSTGRES_PASSWORD")
+end
+
 config :todos_api, TodosApiWeb.Endpoint,
   http: [port: 4002],
   server: false
